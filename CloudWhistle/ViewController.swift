a40c11a27a70ef1d1b0be07034e3aeae133a64df
Prudhvi Gadiraju <gadiraju.prudhvi@gmail.com>
2019-05-02 19:09:13 -0700

    Download from CloudKit

--- CloudWhistle/ViewController.swift
+++ CloudWhistle/ViewController.swift
@@ -7,22 +7,91 @@
 //
 
 import UIKit
+import CloudKit
 
-class ViewController: UIViewController {
+class ViewController: UITableViewController {
     
     static var isDirty = false
+    
+    var whistles = [Whistle]()
+    
 
     // Init
     override func viewDidLoad() {
         super.viewDidLoad()
         
         setupNavBar()
+        tableView.register(UITableViewCell.self, forCellReuseIdentifier: "cell")
+    }
+    
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        
+        if let indexPath = tableView.indexPathForSelectedRow {
+            tableView.deselectRow(at: indexPath, animated: true)
+        }
+        
+//        if ViewController.isDirty {
+            loadWhistles()
+//        }
     }
     
     fileprivate func setupNavBar() {
         navigationItem.title = "What's the Whistle"
         navigationItem.rightBarButtonItem = UIBarButtonItem(barButtonSystemItem: .add, target: self, action: #selector(addWhistle))
         navigationItem.backBarButtonItem = UIBarButtonItem(title: "Home", style: .plain, target: nil, action: nil)
+    }
+    
+    func loadWhistles() {
+        let predicate = NSPredicate(value: true)
+        let sort = NSSortDescriptor(key: "creationDate", ascending: false)
+        
+        let query = CKQuery(recordType: "Whistle", predicate: predicate)
+        query.sortDescriptors = [sort]
+        
+        let operation = CKQueryOperation(query: query)
+        operation.desiredKeys = ["genre", "comments"]
+        operation.resultsLimit = 50
+        
+        var newWhistles = [Whistle]()
+        
+        operation.recordFetchedBlock = { record in
+            let whistle = Whistle()
+            whistle.recordID = record.recordID
+            whistle.genre = record["genre"]
+            whistle.comments = record["comments"]
+            newWhistles.append(whistle)
+        }
+        
+        operation.queryCompletionBlock = { [unowned self] (cursor, error) in
+            DispatchQueue.main.async {
+                if error == nil {
+                    ViewController.isDirty = false
+                    self.whistles = newWhistles
+                    self.tableView.reloadData()
+                } else {
+                    let ac = UIAlertController(title: "Fetch failed", message: "There was a problem fetching the list of whistles; please try again: \(error!.localizedDescription)", preferredStyle: .alert)
+                    ac.addAction(UIAlertAction(title: "OK", style: .default))
+                    self.present(ac, animated: true)
+                }
+            }
+        }
+        
+        CKContainer.default().publicCloudDatabase.add(operation)
+    }
+    
+    func makeAttributedString(title: String, subtitle: String) -> NSAttributedString {
+        let titleAttributes = [NSAttributedString.Key.font: UIFont.preferredFont(forTextStyle: .headline), NSAttributedString.Key.foregroundColor: UIColor.purple]
+        let subtitleAttributes = [NSAttributedString.Key.font: UIFont.preferredFont(forTextStyle: .subheadline)]
+        
+        let titleString = NSMutableAttributedString(string: "\(title)", attributes: titleAttributes)
+        
+        if subtitle.count > 0 {
+            let subtitleString = NSAttributedString(string: "\n\(subtitle)", attributes: subtitleAttributes)
+            titleString.append(subtitleString)
+        }
+        
+        return titleString
     }
 
     
@@ -33,6 +102,19 @@ class ViewController: UIViewController {
         let recordWhistleController = RecordWhistleViewController()
         navigationController?.pushViewController(recordWhistleController, animated: true)
     }
-
+    
+    
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return whistles.count
+    }
+    
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        print(whistles[indexPath.row].comments)
+        let cell = tableView.dequeueReusableCell(withIdentifier: "cell", for: indexPath)
+        cell.accessoryType = .disclosureIndicator
+        cell.textLabel?.attributedText = makeAttributedString(title: whistles[indexPath.row].genre, subtitle: whistles[indexPath.row].comments)
+        cell.textLabel?.numberOfLines = 0
+        return cell
+    }
 }
 
---
+++ CloudWhistle/Whistle.swift
@@ -0,0 +1,18 @@
+//
+//  Whistle.swift
+//  CloudWhistle
+//
+//  Created by Prudhvi Gadiraju on 5/2/19.
+//  Copyright © 2019 Prudhvi Gadiraju. All rights reserved.
+//
+
+import CloudKit
+import UIKit
+
+class Whistle {
+    var recordID: CKRecord.ID!
+    var genre: String!
+    var comments: String!
+    var audio: URL!
+    
+}
